3c7625fac4663f50cdbdc10e5e015c1309c8a6a3
Ben Lee <bendelonlee@gmail.com>
2019-01-23 11:15:35 -0700

    Adds ability to find by timestamps by day

--- app/controllers/merchants/searches_controller.rb
+++ app/controllers/merchants/searches_controller.rb
@@ -1,6 +1,13 @@
 class Merchants::SearchesController < ApplicationController
   def show
     attribute = (Merchant.attribute_names & params.keys).first
-    render json: Merchant.find_by(attribute => params[attribute])
+    case attribute
+    when "created_at", "updated_at"
+      date = Date.parse(params[attribute])
+      time_range = date.beginning_of_day .. date.end_of_day
+      render json: Merchant.where(attribute => time_range).first
+    else
+      render json: Merchant.find_by(attribute => params[attribute])
+    end
   end
 end
--- spec/requests/api/v1/merchant_request_spec.rb
+++ spec/requests/api/v1/merchant_request_spec.rb
@@ -11,7 +11,6 @@ describe 'merchant requests' do
     expect(returned_merchants.count).to eq(3)
   end
   it 'returns a specific merchant' do
-    create_list(:merchant, 3)
     id = create(:merchant).id
 
     get "/merchants/#{id}"
@@ -19,10 +18,19 @@ describe 'merchant requests' do
     expect(response).to be_successful
     returned_merchant = JSON.parse(response.body)
     expect(returned_merchant["id"]).to eq(id)
-
-    expected_merchant = Merchant.first
+  end
+  it 'finds a specific merchant by attribute' do
+    create_list(:merchant, 4)
+    expected_merchant = Merchant.second
 
     get "/merchants/find?name=#{expected_merchant.name}"
+
+    returned_merchant = JSON.parse(response.body)
+    expect(returned_merchant["id"]).to eq(expected_merchant.id)
+
+    expected_merchant = Merchant.last
+    expected_merchant.update(created_at: 5.days.ago)
+    get "/merchants/find?created_at=#{expected_merchant.created_at}"
 
     returned_merchant = JSON.parse(response.body)
     expect(returned_merchant["id"]).to eq(expected_merchant.id)
